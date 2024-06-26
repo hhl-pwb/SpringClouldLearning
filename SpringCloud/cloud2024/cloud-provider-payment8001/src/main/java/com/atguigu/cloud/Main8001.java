7857a3e75ef8c092e44180ba787824ae915e8f5e
WeiboPan <1525695648@qq.com>
2024-06-26 22:55:28 +0800

    demo1

--- SpringCloud/cloud2024/cloud-provider-payment8001/src/main/java/com/atguigu/cloud/Main8001.java
+++ SpringCloud/cloud2024/cloud-provider-payment8001/src/main/java/com/atguigu/cloud/Main8001.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
+//包扫描，不用在没个DAO上再去写@Mapper注解了
 @MapperScan("com.atguigu.cloud.mapper") //import tk.mybatis.spring.annotation.MapperScan
 public class Main8001 {
     public static void main(String[] args) {
