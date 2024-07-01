6d6cb7923e322e6576a1c3c440b1fcd276cc2645
WeiboPan <1525695648@qq.com>
2024-07-01 22:45:39 +0800

    swagger3

--- SpringCloud/cloud2024/cloud-provider-payment8001/src/main/java/com/atguigu/cloud/controller/PayController.java
+++ SpringCloud/cloud2024/cloud-provider-payment8001/src/main/java/com/atguigu/cloud/controller/PayController.java
@@ -20,7 +20,7 @@ public class PayController {
     private PayService payService;
 
     @PostMapping(value = "/pay/add")
-    @Operation(summary = "新增",description = "新增支付支付流水方法，json")
+    @Operation(summary = "新增",description = "新增支付支付流水方法，json串做参数")
     public String addPay(@RequestBody Pay pay) {
         System.out.println(pay.toString());
         int i = payService.add(pay);
@@ -28,11 +28,13 @@ public class PayController {
     }
 
     @DeleteMapping(value = "/pay/del/{id}")
+    @Operation(summary = "删除",description = "删除支付流水方法")
     public Integer deletePay(@PathVariable("id") Integer id) {
         return payService.delete(id);
     }
 
     @PutMapping(value = "/pay/update")
+    @Operation(summary = "修改",description = "修改支付流水方法")
     public String updatePay(@RequestBody PayDTO payDTO) {
         Pay pay = new Pay();
         BeanUtils.copyProperties(payDTO, pay);
@@ -41,6 +43,7 @@ public class PayController {
     }
 
     @GetMapping(value = "/pay/get/{id}")
+    @Operation(summary = "按照ID查流水",description = "查询支付流水方法")
     public Pay getById(@PathVariable("id") Integer id) {
         return payService.getById(id);
     }
