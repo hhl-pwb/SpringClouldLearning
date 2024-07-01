48d62b5feffb9f0d6779ee7996b3248a642a58c3
WeiboPan <1525695648@qq.com>
2024-07-01 23:49:54 +0800

    统一处理

--- SpringCloud/cloud2024/cloud-provider-payment8001/src/main/java/com/atguigu/cloud/controller/PayController.java
+++ SpringCloud/cloud2024/cloud-provider-payment8001/src/main/java/com/atguigu/cloud/controller/PayController.java
@@ -2,6 +2,7 @@ package com.atguigu.cloud.controller;
 
 import com.atguigu.cloud.entities.Pay;
 import com.atguigu.cloud.entities.PayDTO;
+import com.atguigu.cloud.resp.ResultData;
 import com.atguigu.cloud.service.PayService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -14,42 +15,50 @@ import java.util.List;
 
 @RestController
 @Slf4j
-@Tag(name = "支付微服务模块",description = "支付CRUD")
+@Tag(name = "支付微服务模块", description = "支付CRUD")
 public class PayController {
     @Resource
     private PayService payService;
 
     @PostMapping(value = "/pay/add")
-    @Operation(summary = "新增",description = "新增支付支付流水方法，json串做参数")
-    public String addPay(@RequestBody Pay pay) {
+    @Operation(summary = "新增", description = "新增支付支付流水方法，json串做参数")
+    public ResultData<String> addPay(@RequestBody Pay pay) {
         System.out.println(pay.toString());
         int i = payService.add(pay);
-        return "成功插入记录，返回值：" + i;
+//        return "成功插入记录，返回值：" + i;
+        return ResultData.success("成功插入记录，返回值：" + i);
     }
 
     @DeleteMapping(value = "/pay/del/{id}")
-    @Operation(summary = "删除",description = "删除支付流水方法")
-    public Integer deletePay(@PathVariable("id") Integer id) {
-        return payService.delete(id);
+    @Operation(summary = "删除", description = "删除支付流水方法")
+    public ResultData<Integer> deletePay(@PathVariable("id") Integer id) {
+//        return payService.delete(id);
+        int i = payService.delete(id);
+        return ResultData.success(i);
     }
 
     @PutMapping(value = "/pay/update")
-    @Operation(summary = "修改",description = "修改支付流水方法")
-    public String updatePay(@RequestBody PayDTO payDTO) {
+    @Operation(summary = "修改", description = "修改支付流水方法")
+    public ResultData<String> updatePay(@RequestBody PayDTO payDTO) {
         Pay pay = new Pay();
         BeanUtils.copyProperties(payDTO, pay);
         int i = payService.update(pay);
-        return "成功修改记录，返回值：" + i;
+//        return "成功修改记录，返回值：" + i;
+        return ResultData.success("成功修改记录，返回值：" + i);
     }
 
     @GetMapping(value = "/pay/get/{id}")
-    @Operation(summary = "按照ID查流水",description = "查询支付流水方法")
-    public Pay getById(@PathVariable("id") Integer id) {
-        return payService.getById(id);
+    @Operation(summary = "按照ID查流水", description = "查询支付流水方法")
+    public ResultData<Pay> getById(@PathVariable("id") Integer id) {
+//        return payService.getById(id);
+        Pay pay = payService.getById(id);
+        return ResultData.success(pay);
     }
 
     @GetMapping(value = "/pay/getAll")
-    public List<Pay> getAll() {
-        return payService.getAll();
+    public ResultData<List<Pay>> getAll() {
+//        return payService.getAll();
+        List<Pay> payList = payService.getAll();
+        return ResultData.success(payList);
     }
 }
--- SpringCloud/cloud2024/cloud-provider-payment8001/src/main/java/com/atguigu/cloud/entities/Pay.java
+++ SpringCloud/cloud2024/cloud-provider-payment8001/src/main/java/com/atguigu/cloud/entities/Pay.java
@@ -1,5 +1,6 @@
 package com.atguigu.cloud.entities;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 import javax.persistence.Column;
@@ -59,6 +60,7 @@ public class Pay {
      */
     @Column(name = "create_time")
     @Schema(title = "创建时间")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     private Date createTime;
 
     /**
@@ -66,6 +68,7 @@ public class Pay {
      */
     @Column(name = "update_time")
     @Schema(title = "更新时间")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     private Date updateTime;
 
     /**
